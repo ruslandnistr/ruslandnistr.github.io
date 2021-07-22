2b5264847b0c64c7adec3a27d19326f27c1a755c
ruslandnistr <84339068+ruslandnistr@users.noreply.github.com>
2021-07-22 20:15:42 +0300

    initial commit

---
+++ precache-manifest.1eee0d73808a2221b6d71bfb31c8faf9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256f0d54eec299c0e766e17af7fc2797",
+    "url": "/index.html"
+  },
+  {
+    "revision": "122bfa18f0bd5825f07c",
+    "url": "/static/css/main.cb412955.chunk.css"
+  },
+  {
+    "revision": "2ddad47d23e39940b27a",
+    "url": "/static/js/2.adc21a79.chunk.js"
+  },
+  {
+    "revision": "122bfa18f0bd5825f07c",
+    "url": "/static/js/main.4e05676e.chunk.js"
+  },
+  {
+    "revision": "fa27f71bd95d5ca81881",
+    "url": "/static/js/runtime-main.cf8cab74.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "45ab2799ba811ebd8effc6cefaead0e0",
+    "url": "/static/media/down.45ab2799.svg"
+  },
+  {
+    "revision": "2a5cfc3e6cfbfa4a29b113252299520c",
+    "url": "/static/media/up.2a5cfc3e.svg"
+  }
+]);
